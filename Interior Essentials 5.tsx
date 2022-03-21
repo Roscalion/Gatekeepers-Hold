9248a4744d13c5fc8b73ae8c070d62eb428ed38a
Roscalion <Roscalion@users.noreply.github.com>
2022-03-20 21:20:09 -0600

    TSX FILE
    This is a TSX file for Tiled. 
    
    Adding more later.

---
+++ Interior Essentials 5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Practice Map." tilewidth="32" tileheight="32" tilecount="1533" columns="73">
+ <image source="../../../[TILED] MAP PACK/Japanese Interior Essentials 4/Tilemap/MainTileMap.png" width="2336" height="672"/>
+</tileset>
